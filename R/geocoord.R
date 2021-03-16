045d45a898bf0717f43017e208c57e0cf618666e
xiaojunlin <1508664516@qq.com>
2021-03-16 15:56:42 +0800

    update

--- R/geocoord.R
+++ R/geocoord.R
@@ -1,6 +1,5 @@
-#' Title
-#' @title geocoord
-#' @description get coordinate based on address
+#' @title Converting the addresses to coordinates
+#' @description Converting the address to coordinates
 #' @import jsonlite
 #' @import progress
 #' @import dplyr
@@ -13,55 +12,21 @@
 #' @examples
 #' library(amap)
 #' options(amap.key = "xxxxxxxxxxxxxxxxxx")
-#' dgp <- function(n) {
-#'   address <- data.frame(Number= 1:n,
-#'                      address = c("北京大学", "清华大学", "武汉大学", "华中科技大学", "南京大学"))
-#' return(address)
-#' }
-#'
-#' system.time( z1 <- geocoord(dgp(100)$address) )
-#' system.time( z2 <- geocoord(dgp(1000)$address) )
-
-geocoord <- function(address, n = 10){
-  if (length(address) <= 600){
-    query1 <- function(address, n = 10){
-      if (is.null(getOption('amap.key'))) stop("Please fill your key using 'options(amap.key = 'XXXXXXXXXXXXX')' ") # Check key
-      key = getOption('amap.key')
+#' geocoord("your address in Chinese format")
+geocoord <- function(address, n = 10) {
+  if (length(address) <= 600) {
+    query1 <- function(address, n = 10) {
+      if (is.null(getOption("amap.key"))) stop("Please fill your key using 'options(amap.key = 'XXXXXXXXXXXXX')' ")
+      key <- getOption("amap.key")
       df <- as.data.frame(address)
       dat <- slice(df, 0)
       dat$coordinate <- NULL
-      pb <- progress_bar$new(format = "Processing: [:bar] :percent eta: :eta", total =  length(seq(1, nrow(df), by = n)))
+      pb <- progress_bar$new(format = "Processing: [:bar] :percent eta: :eta", total = length(seq(1, nrow(df), by = n)))
       pb$tick(0)
       for (i in seq(1, nrow(df), by = n)) {
         pb$tick(1)
         try({
-          j = i + n - 1
-          tmp <- slice(df, i:j)
-          url <- paste0("https://restapi.amap.com/v3/geocode/geo?address=", paste0(pull(tmp, address), collapse = "|"), "&key=", key, "&batch=true")
-          list <- fromJSON(url)
-          geocodes <- as.data.frame(list$geocodes)
-          coord <- select(geocodes, coordinate = location)
-          tmp <-  bind_cols(tmp, coord)
-          tmp$coordinate <- as.character(tmp$coordinate)
-          dat <- bind_rows(dat, tmp)
-        })
-      }
-      finaldat <-  tidyr::separate(dat, "coordinate", into = c("longitude", "latitude"), sep = ",")
-      finaldat$longitude = as.numeric(finaldat$longitude)
-      finaldat$latitude = as.numeric(finaldat$latitude)
-      return(finaldat)
-    }
-    query1(address, n)
-  } else {
-    if (is.null(getOption('amap.key'))) stop("Please fill your key using 'options(amap.key = 'XXXXXXXXXXXXX')' ") # Check key
-    key = getOption('amap.key')
-    query2 <- function(address, n = 10){
-      df <- as.data.frame(address)
-      dat <- slice(df, 0)
-      dat$coordinate <- NULL
-      for (i in seq(1, nrow(df), by = n)) {
-        try({
-          j = i + n - 1
+          j <- i + n - 1
           tmp <- slice(df, i:j)
           url <- paste0("https://restapi.amap.com/v3/geocode/geo?address=", paste0(pull(tmp, address), collapse = "|"), "&key=", key, "&batch=true")
           list <- fromJSON(url)
@@ -72,21 +37,48 @@ geocoord <- function(address, n = 10){
           dat <- bind_rows(dat, tmp)
         })
       }
-      finaldat <-  tidyr::separate(dat, "coordinate", into = c("longitude", "latitude"), sep = ",")
-      finaldat$longitude = as.numeric(finaldat$longitude)
-      finaldat$latitude  = as.numeric(finaldat$latitude)
+      finaldat <- tidyr::separate(dat, "coordinate", into = c("longitude", "latitude"), sep = ",")
+      finaldat$longitude <- as.numeric(finaldat$longitude)
+      finaldat$latitude <- as.numeric(finaldat$latitude)
+      return(finaldat)
+    }
+    query1(address, n)
+  } else {
+    if (is.null(getOption("amap.key"))) stop("Please fill your key using 'options(amap.key = 'XXXXXXXXXXXXX')' ")
+    key <- getOption("amap.key")
+    query2 <- function(address, n = 10) {
+      df <- as.data.frame(address)
+      dat <- slice(df, 0)
+      dat$coordinate <- NULL
+      for (i in seq(1, nrow(df), by = n)) {
+        try({
+          j <- i + n - 1
+          tmp <- slice(df, i:j)
+          url <- paste0("https://restapi.amap.com/v3/geocode/geo?address=", paste0(pull(tmp, address), collapse = "|"), "&key=", key, "&batch=true")
+          list <- fromJSON(url)
+          geocodes <- as.data.frame(list$geocodes)
+          coord <- select(geocodes, coordinate = location)
+          tmp <- bind_cols(tmp, coord)
+          tmp$coordinate <- as.character(tmp$coordinate)
+          dat <- bind_rows(dat, tmp)
+        })
+      }
+      finaldat <- tidyr::separate(dat, "coordinate", into = c("longitude", "latitude"), sep = ",")
+      finaldat$longitude <- as.numeric(finaldat$longitude)
+      finaldat$latitude <- as.numeric(finaldat$latitude)
       return(finaldat)
     }
     spldata <- split(address, f = ceiling(seq(length(address)) / n))
     cores <- detectCores()
     cl <- makeCluster(cores)
-    result = pblapply(
-      cl = cl, X = 1:length(spldata),
-      FUN = function(i){
-        result = query2(unlist(spldata[[i]]))
+    result <- pblapply(
+      cl = cl, X = seq_len(length(spldata)),
+      FUN = function(i) {
+        result <- query2(unlist(spldata[[i]]))
         return(result)
-      })
-    finaldat = bind_rows(result)
+      }
+    )
+    finaldat <- bind_rows(result)
     return(finaldat)
     stopCluster(cl)
   }
--- R/geomap.R
+++ R/geomap.R
@@ -1,6 +1,5 @@
-#' Title
-#' @title geomap
-#' @description Get the map data with sf format
+#' @title Getting the map data with sf format
+#' @description Getting the map data with sf format
 #' @import sf
 #' @import tidyverse
 #' @param adcode The administrative code
@@ -15,15 +14,15 @@
 #' # (1) fetch and map at province level
 #' china1 <- geomap(adcode = 100000, level = "province")
 #' ggplot(china1) +
-#'  geom_sf(fill = "white") +
-#'  theme_bw()
+#'   geom_sf(fill = "white") +
+#'   theme_bw()
 #' # (2) fetch and map at city level
-#' china2 <- geomap(adcode = 100000,  level = "city")
+#' china2 <- geomap(adcode = 100000, level = "city")
 #' ggplot(china2) +
-#'  geom_sf(fill = "white") +
-#'  theme_bw()
+#'   geom_sf(fill = "white") +
+#'   theme_bw()
 #' # (3) fetch and map at district level
-#' china3 <- geomap(adcode = 100000,  level = "district")
+#' china3 <- geomap(adcode = 100000, level = "district")
 #' ggplot(china3) +
 #'   geom_sf(fill = "white") +
 #'   theme_bw()
@@ -44,7 +43,7 @@
 #' ggplot(sichuan2) +
 #'   geom_sf(fill = "white") +
 #'   theme_bw()
-#' #Note: The argument of level is not allowed for the municipality or special administrative region, please use 'level = district'.
+#' # Note: The argument of level is not allowed for the municipality or special administrative region, please use 'level = district'.
 #' beijing1 <- geomap(adcode = 110000, level = "city")
 #' beijing2 <- geomap(adcode = 110000, level = "district")
 #' ggplot(beijing2) +
@@ -77,179 +76,178 @@
 #' ggplot(chengdu3) +
 #'   geom_sf(fill = "white") +
 #'   theme_bw()
-#'
-#'
-#'
-#'
-
-geomap<- function(adcode, level = "default"){
+geomap <- function(adcode, level = "default") {
   if (adcode == 100000) {
     switch(level,
-           province = {
-             map_province <- read_sf("https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json") %>% select(c(adcode, name, level))
-             map_province <- rename(map_province, "name_province" = "name")
-             map <- map_province
-           },
-           city = {
-             # 省级地图
-             map_province <- read_sf("https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json") %>% select(c(adcode, name, level))
-             map_province <- rename(map_province, "name_province" = "name")
-             tmp <- select(map_province, c(adcode, name_province)) %>% filter(adcode != "100000") # save adcode and province name
-             #十段线
-             dash_line <- filter(map_province, adcode == "100000") %>% select(c(adcode, name_province, level))
-             dash_line$name_city <- NA
-             dash_line$name_district <- NA
-             dash_line1 <- select(dash_line, c(adcode, name_province, name_city, level))
-             dash_line2 <- select(dash_line, c(adcode, name_province, name_city, name_district, level))
-             #台湾
-             taiwan <- filter(map_province, adcode == "710000") %>% select(c(adcode, name_province, level))
-             taiwan$name_city <- NA
-             taiwan$name_district <- NA
-             taiwan1 <- select(taiwan, c(adcode, name_province, name_city, level))
-             taiwan2 <- select(taiwan, c(adcode, name_province, name_city, name_district, level))
-             #地市级地图
-             map_city <- c()
-             for (i in 1:nrow(tmp)){
-               url <- paste0("https://geo.datav.aliyun.com/areas_v2/bound/", tmp$adcode[i], "_full.json")
-               addmap <- read_sf(url) %>% select(c(adcode, name, level))
-               addmap$name_province <- tmp$name_province[i]
-               addmap$name_city <- addmap$name
-               map_city <- rbind(map_city, addmap)
-             }
-             map_city <- select(map_city, c(adcode, name_province, name_city, level))
-             map_city <- rbind(map_city, dash_line1)
-             map <- map_city
-           },
-           district = {
-             # 省级地图
-             map_province <- read_sf("https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json") %>% select(c(adcode, name, level))
-             map_province <- rename(map_province, "name_province" = "name")
-             tmp <- select(map_province, c(adcode, name_province)) # save adcode and province name
-             #十段线
-             dash_line <- filter(map_province, adcode == "100000") %>% select(c(adcode, name_province, level))
-             dash_line$name_city <- NA
-             dash_line$name_district <- NA
-             dash_line1 <- select(dash_line, c(adcode, name_province, name_city, level))
-             dash_line2 <- select(dash_line, c(adcode, name_province, name_city, name_district, level))
-             #台湾
-             taiwan <- filter(map_province, adcode == "710000") %>% select(c(adcode, name_province, level))
-             taiwan$name_city <- NA
-             taiwan$name_district <- NA
-             taiwan1 <- select(taiwan, c(adcode, name_province, name_city, level))
-             taiwan2 <- select(taiwan, c(adcode, name_province, name_city, name_district, level))
-             #地市级地图
-             map_city <- c()
-             for (i in 1:nrow(tmp)){
-               url <- paste0("https://geo.datav.aliyun.com/areas_v2/bound/", tmp$adcode[i], "_full.json")
-               addmap <- read_sf(url) %>% select(c(adcode, name, level))
-               addmap$name_province <- tmp$name_province[i]
-               addmap$name_city <- addmap$name
-               map_city <- rbind(map_city, addmap)
-             }
-             map_city <- select(map_city, c(adcode, name_province, name_city, level))
-             map_city <- rbind(map_city, dash_line1)
-             #区县级地图
-             # (1) 直辖市和特别行政区
-             map_district0 <- map_city %>% filter(level == "district")
-             map_district0$name_district <- map_district0$name_city
-             map_district0$name_city <- NA
-             map_district0 <- select(map_district0, c(adcode, name_province, name_city, name_district, level))
-             #(2) 有区县的城市
-             tmp <- map_city %>% filter(level == "city") %>% select(c(adcode, name_province, name_city))
-             map_district <- c()
-             for (i in 1:nrow(tmp)){
-               url <- paste0("https://geo.datav.aliyun.com/areas_v2/bound/", tmp$adcode[i], "_full.json")
-               addmap <- read_sf(url) %>% select(c(adcode, name, level))
-               addmap$name_province <- tmp$name_province[i]
-               addmap$name_city <- tmp$name_city[i]
-               addmap$name_district <- addmap$name
-               map_district <- rbind(map_district, addmap)
-             }
-             map_district <- select(map_district, c(adcode, name_province, name_city, name_district, level))
-             map_district <- rbind(map_district0, map_district, taiwan2, dash_line2)
-             map <- map_district
-           },
-           default = {
-             map_province <- read_sf("https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json") %>% select(c(adcode, name, level))
-             map_province <- rename(map_province, "name_province" = "name")
-             map <- map_province
-           }
+      province = {
+        map_province <- read_sf("https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json") %>% select(c(adcode, name, level))
+        map_province <- rename(map_province, "name_province" = "name")
+        map <- map_province
+      },
+      city = {
+        # 省级地图
+        map_province <- read_sf("https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json") %>% select(c(adcode, name, level))
+        map_province <- rename(map_province, "name_province" = "name")
+        tmp <- select(map_province, c(adcode, name_province)) %>% filter(adcode != "100000") # save adcode and province name
+        # 十段线
+        dash_line <- filter(map_province, adcode == "100000") %>% select(c(adcode, name_province, level))
+        dash_line$name_city <- NA
+        dash_line$name_district <- NA
+        dash_line1 <- select(dash_line, c(adcode, name_province, name_city, level))
+        dash_line2 <- select(dash_line, c(adcode, name_province, name_city, name_district, level))
+        # 台湾
+        taiwan <- filter(map_province, adcode == "710000") %>% select(c(adcode, name_province, level))
+        taiwan$name_city <- NA
+        taiwan$name_district <- NA
+        taiwan1 <- select(taiwan, c(adcode, name_province, name_city, level))
+        taiwan2 <- select(taiwan, c(adcode, name_province, name_city, name_district, level))
+        # 地市级地图
+        map_city <- c()
+        for (i in 1:nrow(tmp)) {
+          url <- paste0("https://geo.datav.aliyun.com/areas_v2/bound/", tmp$adcode[i], "_full.json")
+          addmap <- read_sf(url) %>% select(c(adcode, name, level))
+          addmap$name_province <- tmp$name_province[i]
+          addmap$name_city <- addmap$name
+          map_city <- rbind(map_city, addmap)
+        }
+        map_city <- select(map_city, c(adcode, name_province, name_city, level))
+        map_city <- rbind(map_city, dash_line1)
+        map <- map_city
+      },
+      district = {
+        # 省级地图
+        map_province <- read_sf("https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json") %>% select(c(adcode, name, level))
+        map_province <- rename(map_province, "name_province" = "name")
+        tmp <- select(map_province, c(adcode, name_province)) # save adcode and province name
+        # 十段线
+        dash_line <- filter(map_province, adcode == "100000") %>% select(c(adcode, name_province, level))
+        dash_line$name_city <- NA
+        dash_line$name_district <- NA
+        dash_line1 <- select(dash_line, c(adcode, name_province, name_city, level))
+        dash_line2 <- select(dash_line, c(adcode, name_province, name_city, name_district, level))
+        # 台湾
+        taiwan <- filter(map_province, adcode == "710000") %>% select(c(adcode, name_province, level))
+        taiwan$name_city <- NA
+        taiwan$name_district <- NA
+        taiwan1 <- select(taiwan, c(adcode, name_province, name_city, level))
+        taiwan2 <- select(taiwan, c(adcode, name_province, name_city, name_district, level))
+        # 地市级地图
+        map_city <- c()
+        for (i in 1:nrow(tmp)) {
+          url <- paste0("https://geo.datav.aliyun.com/areas_v2/bound/", tmp$adcode[i], "_full.json")
+          addmap <- read_sf(url) %>% select(c(adcode, name, level))
+          addmap$name_province <- tmp$name_province[i]
+          addmap$name_city <- addmap$name
+          map_city <- rbind(map_city, addmap)
+        }
+        map_city <- select(map_city, c(adcode, name_province, name_city, level))
+        map_city <- rbind(map_city, dash_line1)
+        # 区县级地图
+        # (1) 直辖市和特别行政区
+        map_district0 <- map_city %>% filter(level == "district")
+        map_district0$name_district <- map_district0$name_city
+        map_district0$name_city <- NA
+        map_district0 <- select(map_district0, c(adcode, name_province, name_city, name_district, level))
+        # (2) 有区县的城市
+        tmp <- map_city %>%
+          filter(level == "city") %>%
+          select(c(adcode, name_province, name_city))
+        map_district <- c()
+        for (i in 1:nrow(tmp)) {
+          url <- paste0("https://geo.datav.aliyun.com/areas_v2/bound/", tmp$adcode[i], "_full.json")
+          addmap <- read_sf(url) %>% select(c(adcode, name, level))
+          addmap$name_province <- tmp$name_province[i]
+          addmap$name_city <- tmp$name_city[i]
+          addmap$name_district <- addmap$name
+          map_district <- rbind(map_district, addmap)
+        }
+        map_district <- select(map_district, c(adcode, name_province, name_city, name_district, level))
+        map_district <- rbind(map_district0, map_district, taiwan2, dash_line2)
+        map <- map_district
+      },
+      default = {
+        map_province <- read_sf("https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json") %>% select(c(adcode, name, level))
+        map_province <- rename(map_province, "name_province" = "name")
+        map <- map_province
+      }
     )
-  } else if (substr(adcode, 3, 4) == "00" & substr(adcode, 1, 2) %in% c(11, 12, 31, 50, 81, 82) == F)  {
+  } else if (substr(adcode, 3, 4) == "00" & substr(adcode, 1, 2) %in% c(11, 12, 31, 50, 81, 82) == F) {
     switch(level,
-           province = {
-             map_province <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,".json")) %>% select(c(adcode, name, level))
-             map_province <- rename(map_province, "name_province" = "name")
-             map <- map_province
-           },
-           city = {
-             map_city <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,"_full.json")) %>% select(c(adcode, name, level))
-             map_city <- rename(map_city, "name_city" = "name")
-             map <- map_city
-           },
-           district = {
-             map_city <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,"_full.json")) %>% select(c(adcode, name, level))
-             map_city <- rename(map_city, "name_city" = "name")
-             tmp <- map_city %>% filter(level == "city") %>% select(c(adcode, name_city))
-             map_district <- c()
-             for (i in 1:nrow(tmp)){
-               url <- paste0("https://geo.datav.aliyun.com/areas_v2/bound/", tmp$adcode[i], "_full.json")
-               addmap <- read_sf(url) %>% select(c(adcode, name, level))
-               addmap$name_city <- tmp$name_city[i]
-               addmap$name_district <- addmap$name
-               map_district <- rbind(map_district, addmap)
-             }
-             map <- map_district
-           },
-           default = {
-             map_city <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,"_full.json")) %>% select(c(adcode, name, level))
-             map_city <- rename(map_city, "name_city" = "name")
-             map <- map_city
-           }
+      province = {
+        map_province <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, ".json")) %>% select(c(adcode, name, level))
+        map_province <- rename(map_province, "name_province" = "name")
+        map <- map_province
+      },
+      city = {
+        map_city <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, "_full.json")) %>% select(c(adcode, name, level))
+        map_city <- rename(map_city, "name_city" = "name")
+        map <- map_city
+      },
+      district = {
+        map_city <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, "_full.json")) %>% select(c(adcode, name, level))
+        map_city <- rename(map_city, "name_city" = "name")
+        tmp <- map_city %>%
+          filter(level == "city") %>%
+          select(c(adcode, name_city))
+        map_district <- c()
+        for (i in 1:nrow(tmp)) {
+          url <- paste0("https://geo.datav.aliyun.com/areas_v2/bound/", tmp$adcode[i], "_full.json")
+          addmap <- read_sf(url) %>% select(c(adcode, name, level))
+          addmap$name_city <- tmp$name_city[i]
+          addmap$name_district <- addmap$name
+          map_district <- rbind(map_district, addmap)
+        }
+        map <- map_district
+      },
+      default = {
+        map_city <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, "_full.json")) %>% select(c(adcode, name, level))
+        map_city <- rename(map_city, "name_city" = "name")
+        map <- map_city
+      }
     )
   } else if (substr(adcode, 3, 4) == "00" & substr(adcode, 1, 2) %in% c(11, 12, 31, 50, 81, 82) == T) {
     switch(level,
-           province = {
-             map_province <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,".json")) %>% select(c(adcode, name, level))
-             map_province <- rename(map_province, "name_province" = "name")
-             map <- map_province},
-           city = {
-             message("The argument of level is not allowed for the municipality or special administrative region, please use 'level = district'.")
-           },
-           district = {
-             map_district <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,"_full.json")) %>% select(c(adcode, name, level))
-             map_district <- rename(map_district, "name_district" = "name")
-             map <- map_district
-           },
-           default = {
-             map_district <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,"_full.json")) %>% select(c(adcode, name, level))
-             map_district <- rename(map_district, "name_district" = "name")
-             map <- map_district
-           }
+      province = {
+        map_province <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, ".json")) %>% select(c(adcode, name, level))
+        map_province <- rename(map_province, "name_province" = "name")
+        map <- map_province
+      },
+      city = {
+        message("The argument of level is not allowed for the municipality or special administrative region, please use 'level = district'.")
+      },
+      district = {
+        map_district <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, "_full.json")) %>% select(c(adcode, name, level))
+        map_district <- rename(map_district, "name_district" = "name")
+        map <- map_district
+      },
+      default = {
+        map_district <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, "_full.json")) %>% select(c(adcode, name, level))
+        map_district <- rename(map_district, "name_district" = "name")
+        map <- map_district
+      }
     )
-  }  else {
+  } else {
     switch(level,
-           province = {
-             message("Error argument of 'level'.")
-           },
-           city = {
-             map_city <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,".json")) %>% select(c(adcode, name, level))
-             map_city <- rename(map_city, "name_city" = "name")
-             map <- map_city
-           },
-           district = {
-             map_district <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,"_full.json")) %>% select(c(adcode, name, level))
-             map_district <- rename(map_district, "name_district" = "name")
-             map <- map_district
-           },
-           default = {
-             map_district <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/",adcode,"_full.json")) %>% select(c(adcode, name, level))
-             map_district <- rename(map_district, "name_district" = "name")
-             map <- map_district
-           }
+      province = {
+        message("Error argument of 'level'.")
+      },
+      city = {
+        map_city <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, ".json")) %>% select(c(adcode, name, level))
+        map_city <- rename(map_city, "name_city" = "name")
+        map <- map_city
+      },
+      district = {
+        map_district <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, "_full.json")) %>% select(c(adcode, name, level))
+        map_district <- rename(map_district, "name_district" = "name")
+        map <- map_district
+      },
+      default = {
+        map_district <- read_sf(paste0("https://geo.datav.aliyun.com/areas_v2/bound/", adcode, "_full.json")) %>% select(c(adcode, name, level))
+        map_district <- rename(map_district, "name_district" = "name")
+        map <- map_district
+      }
     )
   }
 
   return(map)
 }
-
