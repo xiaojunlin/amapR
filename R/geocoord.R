5bae36842ca481f31ff1c7cf9a0c4a013e3130c9
xiaojunlin <1508664516@qq.com>
2021-04-07 15:23:42 +0800

    update geocoord

--- R/geocoord.R
+++ R/geocoord.R
@@ -14,8 +14,6 @@
 #' options(amap.key = "xxxxxxxxxxxxxxxxxx")
 #' geocoord("your address in Chinese format")
 
-
-
 geocoord <- function(address, n = 10) {
 
   trim_address <- function(x){
@@ -31,7 +29,20 @@ geocoord <- function(address, n = 10) {
       key <- getOption("amap.key")
       df <- as.data.frame(address)
       dat <- slice(df, 0)
-      dat$coordinate <- NULL
+      dat <- data.frame(
+        address = NULL,
+        location = NULL,
+        formatted_address = NULL,
+        country = NULL,
+        province = NULL,
+        city = NULL,
+        district = NULL,
+        township = NULL,
+        street = NULL,
+        number = NULL,
+        citycode = NULL,
+        adcode = NULL
+      )
       pb <- progress_bar$new(format = "Processing: [:bar] :percent eta: :eta", total = length(seq(1, nrow(df), by = n)))
       pb$tick(0)
       for (i in seq(1, nrow(df), by = n)) {
@@ -42,16 +53,24 @@ geocoord <- function(address, n = 10) {
           url <- paste0("https://restapi.amap.com/v3/geocode/geo?",
                         "key=", key, "&batch=true",
                         "&address=", trim_address(paste0(pull(tmp, address), collapse = "|"))
-                        )
+          )
           list <- fromJSON(url)
-          geocodes <- as.data.frame(list$geocodes)
-          coord <- select(geocodes, coordinate = location)
-          tmp <- bind_cols(tmp, coord)
-          tmp$coordinate <- as.character(tmp$coordinate)
+          geocode <- list$geocodes %>% select(vars_list)
+          # replace character(0) and list() with NA
+          for (i in vars_list) {
+            geocode[[i]] <- lapply(geocode[[i]],function(x) {
+              if(identical(x, character(0))) NA_character_ else x
+            })
+            geocode[[i]] <- lapply(geocode[[i]],function(x) {
+              if(identical(x, list())) NA_character_ else x
+            })
+          }
+          tmp <- bind_cols(tmp, geocode)
+          tmp$location <- as.character(tmp$location)
           dat <- bind_rows(dat, tmp)
         })
       }
-      finaldat <- tidyr::separate(dat, "coordinate", into = c("longitude", "latitude"), sep = ",")
+      finaldat <- tidyr::separate(dat, "location", into = c("longitude", "latitude"), sep = ",")
       finaldat$longitude <- as.numeric(finaldat$longitude)
       finaldat$latitude <- as.numeric(finaldat$latitude)
       return(finaldat)
@@ -63,7 +82,20 @@ geocoord <- function(address, n = 10) {
     query2 <- function(address, n = 10) {
       df <- as.data.frame(address)
       dat <- slice(df, 0)
-      dat$coordinate <- NULL
+      dat <- data.frame(
+        address = NULL,
+        location = NULL,
+        formatted_address = NULL,
+        country = NULL,
+        province = NULL,
+        city = NULL,
+        district = NULL,
+        township = NULL,
+        street = NULL,
+        number = NULL,
+        citycode = NULL,
+        adcode = NULL
+      )
       for (i in seq(1, nrow(df), by = n)) {
         try({
           j <- i + n - 1
@@ -71,16 +103,24 @@ geocoord <- function(address, n = 10) {
           url <- paste0("https://restapi.amap.com/v3/geocode/geo?",
                         "key=", key, "&batch=true",
                         "&address=", trim_address(paste0(pull(tmp, address), collapse = "|"))
-                        )
+          )
           list <- fromJSON(url)
-          geocodes <- as.data.frame(list$geocodes)
-          coord <- select(geocodes, coordinate = location)
-          tmp <- bind_cols(tmp, coord)
-          tmp$coordinate <- as.character(tmp$coordinate)
+          geocode <- list$geocodes %>% select(vars_list)
+          # replace character(0) and list() with NA
+          for (i in vars_list) {
+            geocode[[i]] <- lapply(geocode[[i]],function(x) {
+              if(identical(x, character(0))) NA_character_ else x
+            })
+            geocode[[i]] <- lapply(geocode[[i]],function(x) {
+              if(identical(x, list())) NA_character_ else x
+            })
+          }
+          tmp <- bind_cols(tmp, geocode)
+          tmp$location <- as.character(tmp$location)
           dat <- bind_rows(dat, tmp)
         })
       }
-      finaldat <- tidyr::separate(dat, "coordinate", into = c("longitude", "latitude"), sep = ",")
+      finaldat <- tidyr::separate(dat, "location", into = c("longitude", "latitude"), sep = ",")
       finaldat$longitude <- as.numeric(finaldat$longitude)
       finaldat$latitude <- as.numeric(finaldat$latitude)
       return(finaldat)
