283e543618e79fc663f22e3887503ca73a6ae455
xiaojunlin <1508664516@qq.com>
2021-04-17 00:09:28 +0800

    update geocoord

--- R/geocoord.R
+++ R/geocoord.R
@@ -2,6 +2,8 @@
 #' @description Converting the address to coordinates
 #' @import jsonlite
 #' @import progress
+#' @import progressr
+#' @import future.apply
 #' @import dplyr
 #' @import parallel
 #' @import pbapply
@@ -20,16 +22,21 @@ geocoord <- function(address, n = 10) {
     x <- gsub("#", "", x)
     x <- gsub(">", "", x)
     x <- gsub("\\s", "", x)
+    x <- gsub("__", "", x)
+    x <- gsub("　", "", x)
     return(x)
   }
 
+  vars_list <-  c('location','formatted_address', 'country', 'province', 'city',
+                  'district', 'township', 'street', 'number', 'citycode', 'adcode')
+
+  if (is.null(getOption("amap.key"))) stop("Please fill your key using 'options(amap.key = 'XXXXXXXXXXXXX')' ")
+  key <- getOption("amap.key")
+
   if (length(address) <= 500) {
     query1 <- function(address, n = 10) {
-      if (is.null(getOption("amap.key"))) stop("Please fill your key using 'options(amap.key = 'XXXXXXXXXXXXX')' ")
-      key <- getOption("amap.key")
       df <- as.data.frame(address)
       dat <- slice(df, 0)
-      dat$coordinate <- NULL
       pb <- progress_bar$new(format = "Processing: [:bar] :percent eta: :eta", total = length(seq(1, nrow(df), by = n)))
       pb$tick(0)
       for (i in seq(1, nrow(df), by = n)) {
@@ -40,28 +47,32 @@ geocoord <- function(address, n = 10) {
           url <- paste0("https://restapi.amap.com/v3/geocode/geo?",
                         "key=", key, "&batch=true",
                         "&address=", trim_address(paste0(pull(tmp, address), collapse = "|"))
-          )
+                        )
           list <- fromJSON(url)
-          geocodes <- as.data.frame(list$geocodes)
-          coord <- select(geocodes, coordinate = location)
-          tmp <- bind_cols(tmp, coord)
-          tmp$coordinate <- as.character(tmp$coordinate)
+          geocode <- list$geocodes %>% select(vars_list)
+
+          for (i in vars_list) {
+            geocode[[i]] <- lapply(geocode[[i]],function(x) {
+              if(identical(x, character(0))) NA_character_ else x
+            })
+            geocode[[i]] <- lapply(geocode[[i]],function(x) {
+              if(identical(x, list())) NA_character_ else x
+            })
+          }
+
+          tmp <- bind_cols(tmp, geocode) %>% mutate_all(as.character)
           dat <- bind_rows(dat, tmp)
         })
       }
-      finaldat <- tidyr::separate(dat, "coordinate", into = c("longitude", "latitude"), sep = ",")
-      finaldat$longitude <- as.numeric(finaldat$longitude)
-      finaldat$latitude <- as.numeric(finaldat$latitude)
-      return(finaldat)
+      result <- tidyr::separate(dat, "location", into = c("longitude", "latitude"), sep = ",") %>%
+        mutate_at(c("longitude", "latitude", "citycode", "adcode"), as.numeric)
+      return(result)
     }
     query1(address, n)
   } else {
-    if (is.null(getOption("amap.key"))) stop("Please fill your key using 'options(amap.key = 'XXXXXXXXXXXXX')' ")
-    key <- getOption("amap.key")
     query2 <- function(address, n = 10) {
       df <- as.data.frame(address)
       dat <- slice(df, 0)
-      dat$coordinate <- NULL
       for (i in seq(1, nrow(df), by = n)) {
         try({
           j <- i + n - 1
@@ -69,32 +80,42 @@ geocoord <- function(address, n = 10) {
           url <- paste0("https://restapi.amap.com/v3/geocode/geo?",
                         "key=", key, "&batch=true",
                         "&address=", trim_address(paste0(pull(tmp, address), collapse = "|"))
-          )
+                        )
           list <- fromJSON(url)
-          geocodes <- as.data.frame(list$geocodes)
-          coord <- select(geocodes, coordinate = location)
-          tmp <- bind_cols(tmp, coord)
-          tmp$coordinate <- as.character(tmp$coordinate)
+          geocode <- list$geocodes %>% select(vars_list)
+
+          for (i in vars_list) {
+            geocode[[i]] <- lapply(geocode[[i]],function(x) {
+              if(identical(x, character(0))) NA_character_ else x
+            })
+            geocode[[i]] <- lapply(geocode[[i]],function(x) {
+              if(identical(x, list())) NA_character_ else x
+            })
+          }
+
+          tmp <- bind_cols(tmp, geocode) %>% mutate_all(as.character)
           dat <- bind_rows(dat, tmp)
         })
       }
-      finaldat <- tidyr::separate(dat, "coordinate", into = c("longitude", "latitude"), sep = ",")
-      finaldat$longitude <- as.numeric(finaldat$longitude)
-      finaldat$latitude <- as.numeric(finaldat$latitude)
-      return(finaldat)
+      result <- tidyr::separate(dat, "location", into = c("longitude", "latitude"), sep = ",") %>%
+        mutate_at(c("longitude", "latitude", "citycode", "adcode"), as.numeric)
+      return(result)
     }
+
     spldata <- split(address, f = ceiling(seq(length(address)) / n))
     cores <- detectCores()
     cl <- makeCluster(cores)
-    result <- pblapply(
-      cl = cl, X = seq_len(length(spldata)),
-      FUN = function(i) {
-        result <- query2(unlist(spldata[[i]]))
-        return(result)
-      }
-    )
-    finaldat <- bind_rows(result)
-    return(finaldat)
-    stopCluster(cl)
+    plan(cluster, workers = cl)
+    xs <- seq_len(length(spldata))
+    handlers(handler_progress(format="[:bar] :percent :eta :message"))
+    with_progress({
+      p <- progressor(along = xs)
+      result <- future_lapply(xs, FUN = function(x){
+        p()
+        query2(unlist(spldata[[x]]))
+      })
+    })
+    results <- bind_rows(result)
+    return(results)
   }
 }
